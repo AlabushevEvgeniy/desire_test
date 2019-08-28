017019a200e2dcd4c1f3b8c4e638350655dd22da
Evgeniy <alabushev89@gmail.com>
2019-08-29 01:16:34 +0500

    users_controller created; index, show, posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,50 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :update, :destroy]
+  # before_action :set_user, only: [:index]
+
+  def index
+    @posts = Post.all
+    # @user_posts = Post.where(id: user.id)
+
+    render json: @posts
+  end
+  #запрошенная новость
+  def show
+    render json: @post
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      render json: @post, status: :created, location: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+      render json: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post.destroy
+  end
+
+  private
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.fetch(:post, {}).permit(
+        :title,
+        :preview,
+        :text,
+        :published)
+    end
+end
