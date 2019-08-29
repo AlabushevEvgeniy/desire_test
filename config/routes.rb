4f221d876a8e5eaf0137bdad331a2ff088143244
Evgeniy <alabushev89@gmail.com>
2019-08-29 21:18:52 +0500

    fix routes devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }
+  devise_for :users
 
   resources :posts
   resources :users, only: [:index, :create, :posts] do
