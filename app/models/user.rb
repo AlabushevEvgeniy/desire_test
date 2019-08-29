54357a0fcc4609d4c186fe0cf6f4739ca7ddc9b9
Evgeniy <alabushev89@gmail.com>
2019-08-29 15:06:18 +0500

    devise is set up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
-models
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, #:registerable,
+         :jwt_authenticatable,
+         jwt_revocation_strategy: JWTBlacklist
+         # :recoverable, :rememberable, :validatable
   has_many :posts
   has_many :favorites
   has_many :favorite_posts, through: :favorites, source: :post
