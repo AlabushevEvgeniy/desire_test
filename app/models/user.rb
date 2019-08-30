353628364adc70e9e6e904cb1b0b9aa1afac38f6
Evgeniy <alabushev89@gmail.com>
2019-08-30 12:29:40 +0500

    delete jwt_blacklist with migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
-         :jwt_authenticatable, jwt_revocation_strategy: JwtBlacklist
+         :jwt_authenticatable, jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null #JwtBlacklist
 
   has_many :posts
   has_many :favorites
