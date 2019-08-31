653c1e6b271277ab5ab0e6e7d5d7e3c426657b26
Evgeniy <alabushev89@gmail.com>
2019-08-31 23:05:59 +0500

    installed rspec

---
+++ spec/requests/request_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :request do
+    describe 'request list of all users' do
+      it 'return 200' do
+      user = User.create(name: 'Test user')
+      get '/api/v1/users'
+      expect(response).to be_successful
+      expect(response.body).to include('Test user')
+    end
+  end
+end
