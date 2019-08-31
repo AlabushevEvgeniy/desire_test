a6d48f1112eff4d0d236426da0a559ebbff38309
Evgeniy <alabushev89@gmail.com>
2019-09-01 00:11:31 +0500

    authentication_spec is ready

---
+++ spec/requests/authentication_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe 'POST /api/v1/users/sign_in', type: :request do
+  let(:user) { User.create!(email: 'foo@bar.com', password: '123123') }
+  let(:url) { '/api/v1/users/sign_in' }
+  let(:params) do
+    {
+      user: {
+        email: user.email,
+        password: user.password
+      }
+    }
+  end
+
+  context 'when params are correct' do
+    before do
+      post url, params: params
+    end
+
+    it 'returns 200' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'returns JTW token in authorization header' do
+      expect(response.headers['Authorization']).to be_present
+    end
+  end
+
+  context 'when login params are incorrect' do
+    before { post url }
+
+    it 'returns unathorized status' do
+      expect(response.status).to eq 401
+    end
+  end
+end
+
+RSpec.describe 'DELETE /api/v1/users/sign_out', type: :request do
+  let(:url) { '/api/v1/users/sign_out' }
+
+  it 'returns 204, no content' do
+    delete url
+    expect(response).to have_http_status(204)
+  end
+end
